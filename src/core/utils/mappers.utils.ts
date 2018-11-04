d7dcd6ff031378318a0aa44c803318593cb31b09
Yan Koshelev <shrampro@gmail.com>
2018-11-04 21:53:49 +0300

    add description field

--- src/core/utils/mappers.utils.ts
+++ src/core/utils/mappers.utils.ts
@@ -1,13 +1,14 @@
 export const mapToItem = (data: any) => {
   return Object.keys(data).reduce((acc, key) => {
-    const { name, isComplete } = data[key];
+    const { name, isComplete, description } = data[key];
 
     return [
       ...acc,
       {
         id: key,
         name,
-        isComplete
+        isComplete,
+        description
       }
     ];
   }, []);
